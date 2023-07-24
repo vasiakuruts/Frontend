987b1774bbb86c9af49f0bafd7a8cd09a961f9f1
vasiakuruts <77865213+vasiakuruts@users.noreply.github.com>
2023-07-24 13:42:21 +0300

    Create sidebar -Settings sidebar

---
+++ src/components/layout/index.tsx
@@ -0,0 +1,25 @@
+import React, { useState } from "react";
+import { ILayout } from "../../common/types/layout";
+import TopBarComponent from "../top-bar";
+import { useLocation } from "react-router-dom";
+import { Box, useMediaQuery } from "@mui/material";
+import SidebarComponent from "../sidebar";
+
+const LayoutComponent = ({ children }: ILayout) => {
+  const [isOpen, setIsOpen] = useState(true);
+  const location = useLocation();
+  const isNonMobile = useMediaQuery("(max-width:600px)");
+  return location.pathname === "/login" || location.pathname === "/register" ? (
+    <>{children}</>
+  ) : (
+    <Box display={isNonMobile ? "flex" : "block"} width="100%" height="100%">
+      <SidebarComponent isNonMobile={isNonMobile} drawerWidth="250" isOpen={isOpen} setIsOpen={setIsOpen} />
+      <Box>
+        <TopBarComponent />
+        {children}
+      </Box>
+    </Box>
+  );
+};
+
+export default LayoutComponent;
