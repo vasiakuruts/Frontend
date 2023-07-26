054c02f8b9ca63095f3a2ac56d81ab6fa4b7958c
vasiakuruts <77865213+vasiakuruts@users.noreply.github.com>
2023-07-26 19:22:58 +0300

    Add responsive top bar - Refactoring top bar component

--- src/components/layout/index.tsx
+++ src/components/layout/index.tsx
@@ -1,24 +1,23 @@
 import React, { useState } from "react";
-import { ILayout } from "../../common/types/layout";
 import TopBarComponent from "../top-bar";
-import { useLocation } from "react-router-dom";
+import { Outlet, useLocation } from "react-router-dom";
 import { Box, useMediaQuery } from "@mui/material";
 import SidebarComponent from "../sidebar";
 import styled from "./styles.module.css";
 
-const LayoutComponent = ({ children }: ILayout) => {
-  const [isOpen, setIsOpen] = useState(true);
+const LayoutComponent = () => {
+  const [isOpen, setIsOpen] = useState(false);
   const location = useLocation();
   const isNonMobile = useMediaQuery("(max-width:600px)");
   
   return location.pathname === "/login" || location.pathname === "/register" ? (
-    <>{children}</>
+    <><Outlet /></>
   ) : (
     <Box display={!isNonMobile ? "flex" : "block"} justifyContent='space-between' width="100%" height="100%">
-      <SidebarComponent isNonMobile={isNonMobile} drawerWidth="250" isOpen={isOpen} setIsOpen={setIsOpen} />
+      <SidebarComponent isNonMobile={isNonMobile} drawerWidth="250px" isOpen={isOpen} setIsOpen={setIsOpen} />
       <Box className={styled.mainSection}>
-        <TopBarComponent />
-        {children}
+        <TopBarComponent isOpen={isOpen} setIsOpen={setIsOpen} />
+        <Outlet />
       </Box>
     </Box>
   );
