31d078e1a1769fc625a2ce148976e4fad370b00a
vasiakuruts <77865213+vasiakuruts@users.noreply.github.com>
2023-07-31 16:02:40 +0300

    Fetch data from coingecko - Create fetch logic - Save data to store - Get data from store in home page

--- src/utils/axios/index.ts
+++ src/utils/axios/index.ts
@@ -5,3 +5,8 @@ export const instance = axios.create({
    timeout: 1000,
    headers: {'X-Custom-Header': 'foobar'}
  });
+ export const coinGeckoApi = axios.create({
+  baseURL: 'https://api.coingecko.com/api/v3',
+  timeout: 1000,
+  headers: {'X-Custom-Header': 'foobar'}
+});
