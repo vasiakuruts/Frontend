31c8501c1112f5fabe5168fad1ac39cd0f44b35b
vasiakuruts <77865213+vasiakuruts@users.noreply.github.com>
2023-08-15 15:50:12 +0300

    Implement  Watchlist logic
    - Create Watchlist component
    - Get data from backend
    - Filtered assets array
    - Render  assets array on watchlist page

--- src/components/top-prace/TopPriceComponent.tsx
+++ src/components/top-prace/TopPriceComponent.tsx
@@ -1,66 +1,8 @@
-import {
-  TableContainer,
-  Table,
-  TableHead,
-  TableRow,
-  TableCell,
-  TableBody,
-  Paper,
-} from "@mui/material";
-import React from "react";
-import styled from "./style.module.css";
+import { AssetsTableComponent } from "../assets-table";
 
 export const TopPriceComponent = (props: any) => {
   const { assets } = props;
 
-  return (
-    <>
-      <TableContainer component={Paper}>
-        <Table sx={{ minWidth: 650 }} aria-label="simple table">
-          <TableHead>
-            <TableRow>
-              <TableCell>Назва</TableCell>
-              <TableCell align="right">Ціна</TableCell>
-              <TableCell align="right">Зміни (%)</TableCell>
-              <TableCell align="right">Зміни ($)</TableCell>
-            </TableRow>
-          </TableHead>
-          <TableBody>
-            {assets.map((element: any) => (
-              <TableRow
-                key={element.name}
-                sx={{ "&:last-child td, &:last-child th": { border: 0 } }}
-              >
-                <TableCell component="th" scope="row">
-                  {element.name}
-                </TableCell>
-                <TableCell align="right">{element.current_price}</TableCell>
-                <TableCell
-                  align="right"
-                  className={
-                    element.price_change_24h > 0
-                      ? `${styled.priceUp}`
-                      : `${styled.priceDown}`
-                  }
-                >
-                  {element.price_change_24h.toFixed(2)}
-                </TableCell>
-                <TableCell
-                  align="right"
-                  className={
-                    element.price_change_percentage_24h > 0
-                      ? `${styled.priceUp}`
-                      : `${styled.priceDown}`
-                  }
-                >
-                  {element.price_change_percentage_24h.toFixed(2)}
-                </TableCell>
-              </TableRow>
-            ))}
-          </TableBody>
-        </Table>
-      </TableContainer>
-    </>
-  );
+  return <AssetsTableComponent assets={assets} />;
 };
 export default TopPriceComponent;
--- src/pages/watchlist/WatchlistPage.tsx
+++ src/pages/watchlist/WatchlistPage.tsx
@@ -1,10 +1,58 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { useAppDispatch, useAppSelector } from "../../utils/hook";
+import { getWatchlistElements } from "../../store/thunks/watchlist";
+import { getTopPriceData } from "../../store/thunks/assets";
+import { AssetsTableComponent } from "../../components/assets-table";
+import { Grid, Typography, useTheme } from "@mui/material";
+import styled from "./styles.module.css";
+import { tokens } from "../../theme";
 
 export const WatchlistPage = () => {
+  const theme = useTheme();
+  const colors = tokens(theme.palette.mode);
+  const dispatch = useAppDispatch();
+  const watchlist = useAppSelector((state) => state.watchlist.assets);
+  const { assets } = useAppSelector((state) => state.assets);
+
+  useEffect(() => {
+    dispatch(getTopPriceData());
+    dispatch(getWatchlistElements());
+  }, [dispatch]);
+
+  const filteredArray = assets.filter((element: any) => {
+    return watchlist.some((otherElement: any) => {
+      return otherElement.assetId === element.id;
+    });
+  });
+
+  console.log(filteredArray);
+
   return (
-    <div>
-      <h1>Watchlist</h1>
-    </div>
+    <Grid className={styled.root}>
+      <Grid className={styled.watchlistHeading}>
+        <Typography variant="h2" className={styled.heading}>
+          Обране
+        </Typography>
+      </Grid>
+      <Grid
+        className={styled.assetsTableBlock}
+        sx={{
+          backgroundColor: `${
+            theme.palette.mode === "light"
+              ? colors.primary.DEFAULT
+              : colors.primary[600]
+          }`,
+          border: `1px solid ${colors.borderColor}`,
+          "& .MuiPaper-root": {
+            backgroundColor: "transparent !important",
+            boxShadow: "none !important",
+            backgroundImage: "none !important",
+          },
+        }}
+      >
+        <AssetsTableComponent assets={filteredArray} />
+      </Grid>
+    </Grid>
   );
 };
 export default WatchlistPage;
